eada8cac4b4d58a84aace75aa6c49e65c2fd97ed
Dongxiao  Huang <dongxiao.huang17@imperial.ac.uk>
2017-11-26 22:58:11 +0000

    add functions.

--- lexicon.pl
+++ lexicon.pl
@@ -12,3 +12,10 @@ verb([kicks]).
 verb([sings]).
 
 animate([cow, boy, girl]).
+
+%turn the encode list into
+to_conn_code([as|c],[asc]).
+to_conn_code([as|b],[asb]).
+to_conn_code([al|g],[alg]).
+to_conn_code([dl|g],[dlg]).
+to_conn_code([dl|s],[dls]).
--- nat.pl
+++ nat.pl
@@ -27,3 +27,76 @@ verb_phrase(VP) :-
 	c.  same_actor(Text)
 
 */
+
+conj(Test) :-
+	append(FirstS,[and|RestT],Test),
+	sentence(FirstS),
+	conj(RestT).
+conj(Test) :- sentence(Test),\+member(and,Test).
+
+%encode()
+
+
+
+
+encoder(Text,Acc,EncodedText) :-
+	Text = [Word|Rest],
+	noun([Word]),
+	animate(Ani),
+	member(Word,Ani),
+	atom_chars(Word,SpitWord),
+	length(SpitWord,Len),
+	Len > 3,!,
+	SpitWord = [FirstC|RestC],
+	EncodedList = [al|FirstC],
+	to_conn_code(EncodedList,EncodedW),
+	append(Acc,EncodedW,AccRes),
+	encoder(Rest,AccRes,EncodedText).
+
+encoder(Text,Acc,EncodedText) :-
+	Text = [Word|Rest],
+	noun([Word]),
+	animate(Ani),
+	member(Word,Ani),
+	atom_chars(Word,SpitWord),
+	length(SpitWord,Len),
+	Len < 4,
+	SpitWord = [FirstC|RestC],
+	EncodedList = [as|FirstC],
+	to_conn_code(EncodedList,EncodedW),
+	append(Acc,EncodedW,AccRes),
+	encoder(Rest,AccRes,EncodedText).
+	encoder(Text,Acc,EncodedText) :-
+		Text = [Word|Rest],
+		noun([Word]),
+		atom_chars(Word,SpitWord),
+		length(SpitWord,Len),
+		Len > 3,
+		SpitWord = [FirstC|RestC],
+		EncodedList = [dl|FirstC],
+		to_conn_code(EncodedList,EncodedW),
+		append(Acc,EncodedW,AccRes),
+		encoder(Rest,AccRes,EncodedText).
+encoder(Text,Acc,EncodedText) :-
+	Text = [Word|Rest],
+	\+noun([Word]),
+	append(Acc,[Word],AccRes),
+	encoder(Rest,AccRes,EncodedText).
+encode(Text,EncodedText) :- encoder(Text,[],EncodedText).
+encoder([],Acc,Acc).
+
+
+%smae_actor(Text)
+same_actor(Text) :-
+	conj(Text),
+	same_actor_acc(Text,Actor).
+%base case
+same_actor_acc(Text, Actor) :-
+	append([the],[Actor|Rest],Text),
+	\+ member(and,Text).
+%
+same_actor_acc(Text, Actor) :-
+	append([the],[Actori|Rest],Text),
+	member(Actori,[Actor]),
+	append(F,[and|ResS],Text),
+	same_actor_acc(ResS, Actor).
